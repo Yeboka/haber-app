52a4f547882d2adcfa93755aedc0ede9b6080f4b
Sanzhar <semid46@gmail.com>
2021-10-30 10:42:47 +0600

    first main.dart scheme

---
+++ lib/core/multilingualism.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class Multilingualism {
+  Multilingualism();
+
+
+}
+
+class Russian extends Multilingualism {
+  List<String> _data = [];
+
+  Russian();
+
+
+
+  List get data => _data;
+}
