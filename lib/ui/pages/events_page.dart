421c23ca6fc1bebedd5403fb942a903896f42686
Sanzhar <semid46@gmail.com>
2021-11-03 20:10:28 +0600

    added simple page in event

--- lib/ui/pages/events_page.dart
+++ lib/ui/pages/events_page.dart
@@ -7,12 +7,35 @@ class EventsPage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _EventsPageState();
   }
-
 }
 
 class _EventsPageState extends State {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+    return ListView(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "Events",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+        ),
+        SizedBox(
+          height: 150,
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("${index + 1} index")
+                );
+              }),
+        ),
+      ],
+    );
   }
 }
