b540a47cdd8287cf4aae166eb954fb11fa750fde
Sanzhar <semid46@gmail.com>
2021-11-06 19:23:21 +0600

    added simple messenger_page.dart, just design

--- lib/ui/pages/messenger_page.dart
+++ lib/ui/pages/messenger_page.dart
@@ -7,15 +7,83 @@ class MessengerPage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _MessengerPageState();
   }
-
 }
 
 class _MessengerPageState extends State {
+  final jsonMap = [
+    {
+      "avatar": "assets/images/icons/red_color.png",
+      "title": "Entrepreneurship",
+      "readIcon": false,
+    },
+    {
+      "avatar": "assets/images/icons/red_color.png",
+      "title": "Computer Since",
+      "readIcon": false,
+    },
+    {
+      "avatar": "assets/images/icons/red_color.png",
+      "title": "SDU Chat",
+      "readIcon": true,
+    },
+    {
+      "avatar": "assets/images/icons/red_color.png",
+      "title": "WOWWO",
+      "readIcon": true,
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
-    );
-  }
+    List<String> title = [];
+    List<String> avatar = [];
+    List<bool> readIcon = [];
+    for (int i = 0; i < jsonMap.length; i++) {
+      for (var element in jsonMap[i].entries) {
+        if (element.key == "title") {
+          title.add(element.value as String);
+        } else if (element.key == "avatar") {
+          avatar.add(element.value as String);
+        } else if (element.key == "readIcon") {
+          readIcon.add(element.value as bool);
+        }
+      }
+    }
 
+    return ListView.builder(
+        itemCount: jsonMap.length,
+        padding: const EdgeInsets.all(5),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2.5),
+            child: Container(
+              height: 65,
+              decoration: BoxDecoration(
+                  // color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Center(
+                child: ListTile(
+                  dense: true,
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(avatar[index]),
+                    //TODO add image when image is error
+                    // onBackgroundImageError: ,
+                    // onForegroundImageError: ,
+                  ),
+                  title: Text(
+                    title[index],
+                    style: const TextStyle(fontSize: 16, color: Colors.blueGrey),
+                  ),
+                  trailing: readIcon[index]
+                      ? null
+                      : const Icon(
+                          Icons.circle,
+                          color: Colors.blueAccent,
+                        ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
 }
